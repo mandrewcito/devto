a8a4af8fb2ff190788425ce8ba48016a437f15f3
mandrewcito <anbaalo@gmail.com>
2020-04-05 21:44:30 +0200

    move console app, create aspnetcore serilog

---
+++ SerilogDotnet/AspnetcoreSerilog/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace AspnetcoreSerilog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            	.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+				    .ReadFrom.Configuration(hostingContext.Configuration))
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ SerilogDotnet/ConsoleDotnetcoreSerilog/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
+
+namespace AspnetcoreSerilog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional:true, reloadOnChange:true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+                
+            Log.Information("Hello world!");
+            Log.Warning("Oops!! ");
+            Log.Error("Ouch!!!! ");
+        }
+    }
+}
